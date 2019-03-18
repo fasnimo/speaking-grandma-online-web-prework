63309940906dee3f6848ee686593083fd0878ff0
Rene Cocom <fasnimo@gmail.com>
2019-03-18 20:03:35 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,5 @@
 
-def speak_to_grandma(grandson = "I LOVE YOU GRANDMA!")
+def speak_to_grandma(require_relative = "I LOVE YOU GRANDMA!")
   if grandson == "I LOVE YOU GRANDMA!".upcase
     puts "I LOVE YOU TOO PUMPKIN!".upcase
   elsif grandson == "Hi Nana, how are you?"
