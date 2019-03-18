94586da6c4703db49a62b3761b553127ba5c57cc
Rene Cocom <fasnimo@gmail.com>
2019-03-18 20:04:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,14 +1,14 @@
 
 def speak_to_grandma(require_relative = "I LOVE YOU GRANDMA!")
-  if grandson == "I LOVE YOU GRANDMA!".upcase
+  if require_relative == "I LOVE YOU GRANDMA!".upcase
     puts "I LOVE YOU TOO PUMPKIN!".upcase
-  elsif grandson == "Hi Nana, how are you?"
+  elsif require_relative == "Hi Nana, how are you?"
     puts "HUH?! SPEAK UP, SONNY!".upcase
-  elsif grandson == "Hi!".upcase
+  elsif require_relative == "Hi!".upcase
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif grandson == "WHAT DID YOU EAT TODAY?".upcase
+  elsif require_relative == "WHAT DID YOU EAT TODAY?".upcase
     puts "NO, NOT SINCE 1938!"upcase
-  elsif grandson == "WHAT?".upcase
+  elsif require_relative == "WHAT?".upcase
     puts "NO, NOT SINCE 1938!".upcase
   else 
     puts "I LOVE YOU TOO PUMPKIN!" .upcase
